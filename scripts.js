a0e3438a13323ee8afb1bfb296f70a0ba3804c03
Rassan99 <99618618+Rassan99@users.noreply.github.com>
2025-01-09 14:40:14 +0300

    Feat: Game Works fully in the console

--- scripts.js
+++ scripts.js
@@ -1,16 +1,65 @@
-const GameBoard = (function () {
-  const board = [
+let GameBoard = (function () {
+  let board = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
   ];
+
   return board;
+})();
+let number = (function () {
+  let num = [];
+  return num;
 })();
 function Player(n, x) {
   let name = n;
   let xO = x;
   return { name, xO };
 }
+function gameController(p1, p2) {
+  let board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  let activePlayer = p1;
+  let switchPlayer = () => {
+    if (activePlayer === p1) {
+      activePlayer = p2;
+    } else {
+      activePlayer = p1;
+    }
+  };
+
+  const printNewRound = () => {
+    console.log(GameBoard);
+    console.log(`${activePlayer.name}'s turn.`);
+  };
+  const playRound = (num) => {
+    console.log(
+      `Putting ${activePlayer.name}'s ${activePlayer.xO} into square ${num}...`
+    );
+    if (changeBoard(num, activePlayer)) {
+      number.push(num);
+      if (checkWins()) {
+        number = [];
+        GameBoard = board;
+        console.log(` ${activePlayer.name}'s ${activePlayer.xO} Won!`);
+      }
+      if (number.length == 9) {
+        number = [];
+        GameBoard = board;
+        console.log(` it's a Draw!`);
+      }
+      switchPlayer();
+      printNewRound();
+    }
+  };
+  return {
+    playRound,
+  };
+}
+
 function changeBoard(num, p1) {
   let result = [-1, -1];
   let pickedNumber = num;
@@ -23,36 +72,43 @@ function changeBoard(num, p1) {
       }
     }
   }
-  GameBoard[result[0]][result[1]] = xO;
-  let winners = checkWins(GameBoard);
-  if (winners.includes("wins")) {
-    console.log(winners);
+  if (number.includes(pickedNumber)) {
+    return false;
+  } else {
+    GameBoard[result[0]][result[1]] = xO;
+    return true;
   }
-  console.log(GameBoard);
 }
 
-function checkWins(board) {
-  if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-    return board[0][0] + " wins";
-  } else if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-    return board[0][2] + " wins";
+function checkWins() {
+  if (
+    GameBoard[0][0] === GameBoard[1][1] &&
+    GameBoard[1][1] === GameBoard[2][2]
+  ) {
+    return true;
+  } else if (
+    GameBoard[0][2] === GameBoard[1][1] &&
+    GameBoard[1][1] === GameBoard[2][0]
+  ) {
+    return true;
   }
 
   for (let i = 0; i < 3; i++) {
-    if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-      return board[i][0] + " wins";
+    if (
+      GameBoard[i][0] === GameBoard[i][1] &&
+      GameBoard[i][1] === GameBoard[i][2]
+    ) {
+      return true;
     }
   }
 
   for (let j = 0; j < 3; j++) {
-    if (board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
-      return board[0][j] + " wins";
+    if (
+      GameBoard[0][j] === GameBoard[1][j] &&
+      GameBoard[1][j] === GameBoard[2][j]
+    ) {
+      return true;
     }
   }
-  return "nothing";
+  return false;
 }
-let rass = Player("rass", "x");
-let ra = Player("ra", "o");
-changeBoard(9, rass);
-changeBoard(5, rass);
-changeBoard(1, rass);
