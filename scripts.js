c678a5332bb27828fa3fe256f0ec2678fd222fff
Rassan99 <99618618+Rassan99@users.noreply.github.com>
2025-01-15 13:01:32 +0300

    Feat:Disply controller works

--- scripts.js
+++ scripts.js
@@ -4,8 +4,14 @@ let GameBoard = (function () {
     [4, 5, 6],
     [7, 8, 9],
   ];
-
-  return board;
+  const getBoard = () => board;
+  let resetBoard = () =>
+    (board = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ]);
+  return { getBoard, resetBoard };
 })();
 let number = (function () {
   let num = [];
@@ -17,11 +23,11 @@ function Player(n, x) {
   return { name, xO };
 }
 function gameController(p1, p2) {
-  let board = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ];
+  // let board = [
+  //   [1, 2, 3],
+  //   [4, 5, 6],
+  //   [7, 8, 9],
+  // ];
   let activePlayer = p1;
   let switchPlayer = () => {
     if (activePlayer === p1) {
@@ -37,19 +43,32 @@ function gameController(p1, p2) {
     console.log(`${activePlayer.name}'s turn.`);
   };
   const playRound = (num) => {
+    const headerDiv = document.querySelector(".header");
+    headerDiv.textContent = "";
+    console.log("length of number =" + number);
     console.log(
       `Putting ${activePlayer.name}'s ${activePlayer.xO} into square ${num}...`
     );
     if (changeBoard(num, activePlayer)) {
-      number.push(num);
+      if (typeof num === "number") {
+        number.push(num);
+      }
       if (checkWins()) {
         number = [];
-        GameBoard = board;
+        GameBoard.resetBoard();
+        const winner = document.createElement("p");
+        winner.id = "winner";
+        winner.textContent = ` ${activePlayer.name}'s ${activePlayer.xO} Won!`;
+        headerDiv.appendChild(winner);
         console.log(` ${activePlayer.name}'s ${activePlayer.xO} Won!`);
       }
       if (number.length == 9) {
         number = [];
-        GameBoard = board;
+        GameBoard.resetBoard();
+        const draw = document.createElement("p");
+        draw.id = "draw";
+        draw.textContent = ` it's a Draw!!`;
+        headerDiv.appendChild(draw);
         console.log(` it's a Draw!`);
       }
       switchPlayer();
@@ -62,44 +81,46 @@ function gameController(p1, p2) {
     getActivePlayer,
   };
 }
-
 function changeBoard(num, p1) {
   let result = [-1, -1];
   let pickedNumber = num;
   const xO = p1.xO;
-  for (let i = 0; i < GameBoard.length; i++) {
-    for (let j = 0; j < GameBoard[i].length; j++) {
-      if (GameBoard[i][j] == pickedNumber) {
+  for (let i = 0; i < GameBoard.getBoard().length; i++) {
+    for (let j = 0; j < GameBoard.getBoard()[i].length; j++) {
+      if (GameBoard.getBoard()[i][j] == pickedNumber) {
         result[0] = i;
         result[1] = j;
       }
     }
   }
-  if (number.includes(pickedNumber)) {
+  // if (typeof pickedNumber != "number") {
+  //   return false;
+  // number.includes(pickedNumber) || }
+  if (isNaN(num)) {
     return false;
   } else {
-    GameBoard[result[0]][result[1]] = xO;
+    GameBoard.getBoard()[result[0]][result[1]] = xO;
     return true;
   }
 }
 
 function checkWins() {
   if (
-    GameBoard[0][0] === GameBoard[1][1] &&
-    GameBoard[1][1] === GameBoard[2][2]
+    GameBoard.getBoard()[0][0] === GameBoard.getBoard()[1][1] &&
+    GameBoard.getBoard()[1][1] === GameBoard.getBoard()[2][2]
   ) {
     return true;
   } else if (
-    GameBoard[0][2] === GameBoard[1][1] &&
-    GameBoard[1][1] === GameBoard[2][0]
+    GameBoard.getBoard()[0][2] === GameBoard.getBoard()[1][1] &&
+    GameBoard.getBoard()[1][1] === GameBoard.getBoard()[2][0]
   ) {
     return true;
   }
 
   for (let i = 0; i < 3; i++) {
     if (
-      GameBoard[i][0] === GameBoard[i][1] &&
-      GameBoard[i][1] === GameBoard[i][2]
+      GameBoard.getBoard()[i][0] === GameBoard.getBoard()[i][1] &&
+      GameBoard.getBoard()[i][1] === GameBoard.getBoard()[i][2]
     ) {
       return true;
     }
@@ -107,8 +128,8 @@ function checkWins() {
 
   for (let j = 0; j < 3; j++) {
     if (
-      GameBoard[0][j] === GameBoard[1][j] &&
-      GameBoard[1][j] === GameBoard[2][j]
+      GameBoard.getBoard()[0][j] === GameBoard.getBoard()[1][j] &&
+      GameBoard.getBoard()[1][j] === GameBoard.getBoard()[2][j]
     ) {
       return true;
     }
@@ -118,6 +139,7 @@ function checkWins() {
 
 function ScreenController() {
   let submitBttn = document.getElementById("submit");
+  let form = document.getElementById("forma");
   submitBttn.addEventListener("click", func);
   const boardDiv = document.querySelector(".board");
   const playerTurnDiv = document.querySelector(".turn");
@@ -129,24 +151,26 @@ function ScreenController() {
   let p2Name;
   let game;
   function func() {
-    choice = document.getElementsByName("choice");
-    for (let i = 0; i < choice.length; i++) {
-      if (choice[i].checked) {
-        p1choice = choice[i].value;
+    if (form[0].checkValidity()) {
+      choice = document.getElementsByName("choice");
+      for (let i = 0; i < choice.length; i++) {
+        if (choice[i].checked) {
+          p1choice = choice[i].value;
+        }
       }
-    }
-    p1Name = document.getElementById("p1name").value;
-    p2Name = document.getElementById("p2name").value;
-    p2choice = p1choice === "x" ? "o" : "x";
-    document.getElementById("form").remove();
-    game = gameController(Player(p1Name, p1choice), Player(p2Name, p2choice));
-    boardDiv.style.display = "grid";
-    const activePlayer = game.getActivePlayer();
-    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
-  }
 
+      p1Name = document.getElementById("p1name").value;
+      p2Name = document.getElementById("p2name").value;
+      p2choice = p1choice === "x" ? "o" : "x";
+      document.getElementById("form").remove();
+      game = gameController(Player(p1Name, p1choice), Player(p2Name, p2choice));
+      boardDiv.style.display = "grid";
+      const activePlayer = game.getActivePlayer();
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+    }
+  }
   const updateScreen = () => {
-    const board = GameBoard;
+    const board = GameBoard.getBoard();
     boardDiv.textContent = "";
 
     board.forEach((row) => {
@@ -161,10 +185,12 @@ function ScreenController() {
   };
 
   function clickHandlerBoard(e) {
-    const selectedColumn = e.target.textContent;
+    const selectedColumn = parseInt(e.target.textContent, 10);
     const activePlayer = game.getActivePlayer();
     playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
     game.playRound(selectedColumn);
+    console.log("the display choice " + selectedColumn);
+
     updateScreen();
   }
   boardDiv.addEventListener("click", clickHandlerBoard);
